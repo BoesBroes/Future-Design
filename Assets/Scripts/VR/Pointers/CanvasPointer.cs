0e49c453f7bf51e602f41dfde91ce8fce03d7cfa
BoesBroes <jonathan.plasman@gmail.com>
2022-01-16 13:48:54 +0100

    VR shit

--- Assets/Scripts/VR/Pointers/CanvasPointer.cs
+++ Assets/Scripts/VR/Pointers/CanvasPointer.cs
@@ -2,7 +2,78 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CanvasPointer : Pointer
+public class CanvasPointer : MonoBehaviour
 {
- 
+    public float defaultLength = 1000f;
+
+    public EventSystem eventSystem;
+    public StandaloneInputModule inputModule;
+
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLength();
+    }
+
+    private void UpdateLength()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, GetEnd());
+    }
+
+    private Vector3 GetEnd()
+    {
+        float distance = GetCanvasDistance();
+        Vector3 endPosition = CalculateEnd(defaultLength);
+
+        if(distance != 0.0f)
+        {
+            endPosition = CalculateEnd(distance);
+        }
+
+        return endPosition;
+    }
+
+    private float GetCanvasDistance()
+    {
+        //get data
+        PointerEventData eventData = new PointerEventData(eventSystem);
+        //eventData.position = inputModule.inputOverride.mousePosition;
+
+        //raycast w data
+        List<RaycastResult> results = new List<RaycastResult>();
+        eventSystem.RaycastAll(eventData, results);
+
+        //get closest
+        RaycastResult closestResult = FindFirstRayCast(results); 
+        float distance = closestResult.distance;
+
+        //clamp
+        distance = Mathf.Clamp(distance, 0.0f, defaultLength);
+        return distance;
+    }
+
+    private RaycastResult FindFirstRayCast(List<RaycastResult> results)
+    {
+        foreach (RaycastResult result in results)
+        {
+            if (!result.gameObject)
+                continue;
+
+            return result;
+        }
+
+        return new RaycastResult();
+    }
+
+    private Vector3 CalculateEnd(float lenght)
+    {
+        return transform.position + (transform.forward * lenght);
+    }    
 }
--- Assets/Scripts/VR/VRInput.cs
+++ Assets/Scripts/VR/VRInput.cs
@@ -2,8 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class VRInput : BaseInput
 {
 
+    XRController controller;
+    public Camera eventCamera;
+
+    public static UnityEvent OnGripPressed = new UnityEvent();
+
+    
+
+
+    //public unityev
+
+    protected override void Start()
+    {
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+
+        Debug.Log(" lol");
+
+        foreach(var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+        }
+    }
+
+    //public override bool GetMouseButton(int button)
+    //{
+    //    //return XR.Input.Common
+    //    //      return InputSystem.GetDevice<XRController>()(UnityEngine.XR.CommonUsages.primary2DAxisClick);
+    //    //return (UnityEngine.XR.CommonUsages.primary2DAxisClick);
+
+    //}
 }
